9dbb6f441ace41b876a6dba3f38dfe737b77a861
ALAN ALTO <alan.alto@fatec.sp.gov.br>
2024-06-23 19:49:26 -0300

    Criação do Serviço de Autenticação

--- StockApp.Infra.Data/Identity/IAuthenticate.cs
+++ StockApp.Infra.Data/Identity/IAuthenticate.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace StockApp.Infra.Data.Identity
 {
-    public class ApplicationUser : IdentityUser
+    internal interface IAuthenticate
     {
     }
 }
